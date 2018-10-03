772c543236df35a79ef94845e9badb4435566906
JasonNgo97 <ngojason1997>
2018-10-02 20:44:21 -0700

    Class Structure Complete

---
+++ deformedBn.py
@@ -0,0 +1,9 @@
+import numpy
+class deformedBn:
+
+    BnTop = None;
+    BnBottom = None;
+    VertexActivated = None;
+    VertexSignaled = None;
+    OriginalLineSet = None;
+    DeformedLineSet = None;
